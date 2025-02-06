be306f0896b71172da27869d9a9cf2a8eb067e99
Tritonn204 <tritonn204@gmail.com>
2025-02-06 00:45:14 -0800

    corrected pluginClass

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <xelis_flutter/xelis_flutter_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  XelisFlutterPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("XelisFlutterPluginCApi"));
+}
