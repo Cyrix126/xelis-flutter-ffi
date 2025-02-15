3c4cb2aaa4be6eae07c20cfd6937de98afc3c1e1
Anthony Tritonn <tritonn204@gmail.com>
2025-02-14 22:57:09 -0800

    alternate dummy handling attempt

--- macos/Classes/XelisFlutterPlugin.swift
+++ macos/Classes/XelisFlutterPlugin.swift
@@ -2,6 +2,13 @@ import Cocoa
 import FlutterMacOS
 
 public class XelisFlutterPlugin: NSObject, FlutterPlugin {
+
+  @_cdecl("dummy_method_to_enforce_bundling")
+  func dummy_method_to_enforce_bundling() -> Int64 {
+      // This exists just to force the linker to include our Rust library
+      return 42
+  }
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "xelis_flutter", binaryMessenger: registrar.messenger)
     let instance = XelisFlutterPlugin()
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,7 +2,8 @@ pub mod api;
 
 #[no_mangle]
 pub extern "C" fn dummy_method_to_enforce_bundling() -> i64 {
-    42 // Return any fixed value, it's just to force linking
+    42
 }
 
+pub use flutter_rust_bridge::*;
 mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
